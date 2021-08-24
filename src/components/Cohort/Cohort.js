67b935fd94e213406c73dcb363ab05f9831080ba
Hayley Witherell <hayleywitherell@gmail.com>
2021-08-23 18:11:04 -0600

    Get props working

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -4,7 +4,7 @@ import './Cohort.css';
 
 function Cohort() {
   return (
-    <Person />
+    <Person pic='https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2020%2F10%2F13%2Fcorgi-dog-POPDOGNAME1020.jpg' likes='likes walks' trait='MUCH SLOBBER' />
   );
 }
 
