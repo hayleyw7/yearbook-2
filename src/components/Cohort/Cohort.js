0518c8dce285ac649bb9fc29f157566e3cf4b90e
Hayley Witherell <hayleywitherell@gmail.com>
2021-08-23 18:19:32 -0600

    Get 2 cards working with props

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -4,8 +4,14 @@ import './Cohort.css';
 
 function Cohort() {
   return (
-    <Person pic='https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2020%2F10%2F13%2Fcorgi-dog-POPDOGNAME1020.jpg' likes='likes walks' trait='MUCH SLOBBER' />
-  );
+    <div class='flexible'>
+
+      <Person name='Dog' pic='https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2020%2F10%2F13%2Fcorgi-dog-POPDOGNAME1020.jpg' likes='likes walks' trait='MUCH SLOBBER' />
+
+      <Person name='Cat' pic='https://i.natgeofe.com/n/f0dccaca-174b-48a5-b944-9bcddf913645/01-cat-questions-nationalgeographic_1228126.jpg' likes='likes sunbathing' trait='SO SNUGGLY' />      
+      
+    </div>
+  )
 }
 
 export default Cohort;
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -3,20 +3,12 @@ import './Person.css';
 
 function Person(props) {
   return (
-    <div class='flexible'>
       <section>
         <img src={props.pic}></img>
-        <h2>Dog</h2>
+        <h2>{props.name}</h2>
         <p class='likes'>{props.likes}</p>
         <p class='trait'>{props.trait}</p>        
       </section>
-    </div>
-
-
-
-
-
-
   );
 }
 
